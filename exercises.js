ef9968d0fec8a4173baa16f18e5e22153faaef2c
Peter <peter@Peters-MacBook-Pro.local>
2018-07-09 22:02:10 -1000

    I will enjoy this

--- exercises.js
+++ exercises.js
@@ -11,9 +11,12 @@ Write a for-loop that will iterate through 20 numbers (starting at 1 and ending 
 "Now serving 20."
 */
 
+for(var i = 1; i<21; i++){
+  console.log("Now serving " + i);
+}
 
 /* 2) Pop Charts
-Write a for-loop that will iterate through the topFive array below and console.log the following message:
+Write a for-loop that will iterateˀ through the topFive array below and console.log the following message:
 This week's chart buster is: 'Closer.'
 This week's chart buster is: 'Starboy.'
 This week's chart buster is: 'I Feel It Coming.'
